3787debd0543a787ff94b2594aea938c5b40b19e
jaehyunlee <jaehyunlee25@gmail.com>
2022-05-21 21:47:40 +0900

    'renew'

--- script/parkvalley.js
+++ script/parkvalley.js
@@ -1,1 +1,5 @@
-(() => {})()
+(() => {
+    ctl00_contents_userID.value = '${login_id}';
+    ctl00_contents_userPassword.value = '${login_password}';
+    LoginButton.click();
+})()
--- script/players.js
+++ script/players.js
@@ -1,1 +1,6 @@
-(() => {})()
+(() => {
+    const ipts = subcontent.getElementsByTagName("input");
+    ipts[0].value = '${login_id}';
+    ipts[2].value = '${login_password}';
+    ipts[1].click();
+})()
