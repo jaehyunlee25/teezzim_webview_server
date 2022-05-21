b08ad8ecc45f0bc8899dec3e8eddd0f5991662e3
jaehyunlee <jaehyunlee25@gmail.com>
2022-05-21 22:27:12 +0900

    'renew'

--- script/uni_island.js
+++ script/uni_island.js
@@ -1,1 +1,5 @@
-(() => {})()
+(() => {
+    ctl00_ContentPlaceHolder1_UserID.value = '${login_id}';
+    ctl00_ContentPlaceHolder1_UserPassword.value = '${login_password}';
+    btnLogin.click();
+})()
