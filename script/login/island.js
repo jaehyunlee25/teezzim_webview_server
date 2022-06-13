4c49ab22fdfad35144389b5431bae380cdf55951
jaehyunlee <jaehyunlee25@gmail.com>
2022-06-14 01:00:08 +0900

    renew

--- script/login/island.js
+++ script/login/island.js
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
-(()=>{
-    document.getElementById('loginId').value = '${login_id}';
-    document.getElementById('loginPw').value = '${login_password}';
-    Data_Save();
-})();
-=======
+
+
+
+
+
+
+
 document.getElementById('loginId').value = '${login_id}';
 document.getElementById('loginPw').value = '${login_password}';
 Data_Save();
->>>>>>> 8c2f3cb04c4c5343d3420d8f9bd200fdd5f2f6b7
+
