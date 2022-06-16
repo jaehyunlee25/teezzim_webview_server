d759646155d248f0e21b135aa666a9bcd6e39992
jaehyunlee <jaehyunlee25@gmail.com>
2022-06-16 18:27:52 +0900

    'renew'

--- teezzim.js
+++ teezzim.js
@@ -125,7 +125,10 @@ function procPost(request, response, data) {
     });
   } else if (request.url == "/get_pure_search_core") {
     const engName = data.club;
-    const core = fs.readFileSync("script/search/" + engName + ".js", "utf-8");
+    const core = fs.readFileSync(
+      "script/search_cor/" + engName + ".js",
+      "utf-8"
+    );
     response.write(JSON.stringify({ core }));
     response.end();
   } else if (request.url == "/search") {
