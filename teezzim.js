631f1ac8961ab88ea48a4c459be422b3a3ab998d
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-05 00:11:25 +0900

    'renew'

--- teezzim.js
+++ teezzim.js
@@ -274,6 +274,10 @@ function procPost(request, response, data) {
     const engName = data.club;
     const sql = "getDeviceByClub.sql".gfdp({ engName });
     sql.query((err, rows, fields) => {
+      if (rows.length === 0) {
+        controlForAdminDevice();
+        return;
+      }
       const top = rows[0];
       const std = new Date() - top.created_at;
       const m5 = 1000 * 60 * 5;
