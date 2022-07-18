467f17145e40cd0b037bef2c90b3f5d15e210761
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-18 19:04:52 +0900

    'renew'

--- script/reserve/search/eodeungsan.js
+++ script/reserve/search/eodeungsan.js
@@ -29,12 +29,10 @@ javascript:(() => {
             result,
         };
         const addr = OUTER_ADDR_HEADER + "/api/reservation/newReserveSearch";
-            post(addr, param, { "Content-Type": "application/json" }, (data) => {
-                console.log(data);
-                const ac = window.AndroidController;
-                if (ac) ac.message("end of reserve/search");
-            });
-            
-        funcReserve = () => {};
+        post(addr, param, { "Content-Type": "application/json" }, (data) => {
+            console.log(data);
+            const ac = window.AndroidController;
+            if (ac) ac.message("end of reserve/search");
+        });
     };
 })();
