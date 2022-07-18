f6ddaa02a599b4da50682fad5640bce938b04ee6
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-18 19:18:49 +0900

    'renew'

--- script/reserve/search/eodeungsan.js
+++ script/reserve/search/eodeungsan.js
@@ -1,18 +1,12 @@
 javascript:(() => {
     ${commonScript}
     const addr = location.href;
-    const year = "${year}";
-    const month = "${month}";
-    const date = "${date}";
-    const course = "${course}";
-    const time = "${time}";
     const dict = {
         "${loginUrl}": funcLogin,
         "${reserveUrl}": funcReserve,
     };
     const func = dict[addr];    
-    if(!func) location.href = "${reserveUrl}";
-    else func();
+    if(func) func();
     function funcLogin() {
         ${loginScript}
     };
