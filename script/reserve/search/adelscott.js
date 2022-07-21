2602ff6a992d1ba32ff65ca6495cdfa52a80ddf1
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-21 22:58:24 +0900

    'renew'

--- script/reserve/search/adelscott.js
+++ script/reserve/search/adelscott.js
@@ -1,5 +1,5 @@
 javascript: (() => {
-  ${commonScript}
+  //${commonScript}
   const addr = location.href.split("#")[0];
   const dict = {
     "${loginUrl}": funcLogin,
@@ -9,9 +9,43 @@ javascript: (() => {
   if (!func) location.href = "${reserveUrl}";
   else func();
   function funcLogin() {
-    ${loginScript}
+    //${loginScript}
   }
   function funcReserve() {
-    log(11);
-  }  
+    const els = document
+      .getElementsByClassName("cm_time_list_tbl")[0]
+      .getElementsByClassName("cm_btn gray");
+    const result = [];
+    const dictCourse = {
+      1: "Mountain",
+      2: "Hill",
+      3: "Lake",
+    };
+    Array.from(els).forEach((el) => {
+      const [, , date, time, course] = el.getAttribute("onclick").inparen();
+      console.log("reserve search", dictCourse[course], date, time);
+      result.push({ date, time, course: dictCourse[course] });
+      const param = {
+        golf_club_id: "${golfClubId}",
+        result,
+      };
+      const addr = OUTER_ADDR_HEADER + "/api/reservation/newReserveSearch";
+      post(addr, param, { "Content-Type": "application/json" }, (data) => {
+        console.log(data);
+        const param = {
+          type: "command",
+          sub_type: "reserve/search",
+          device_id: "${deviceId}",
+          device_token: "${deviceToken}",
+          golf_club_id: "${golfClubId}",
+          message: "end of reserve/search",
+          parameter: JSON.stringify({}),
+        };
+        TZLOG(param, (data) => {
+          log(data);
+          location.href = "/login/logout.asp";
+        });
+      });
+    });
+  }
 })();
