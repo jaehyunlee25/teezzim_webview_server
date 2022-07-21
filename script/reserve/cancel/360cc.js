1abb2b272f79ee074b5a87c8ee62cd935ae56516
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-21 11:47:33 +0900

    'renew'

---
+++ script/reserve/cancel/360cc.js
@@ -0,0 +1,37 @@
+javascript: (() => {
+  ${commonScript}
+  const addr = location.href;
+  const year = "${year}";
+  const month = "${month}";
+  const date = "${date}";
+  const course = "${course}";
+  const time = "${time}";
+  const dict = {
+    "${loginUrl}": funcLogin,
+    "${reserveUrl}": funcCancel,
+  };
+  const func = dict[addr];
+  if (func) func();
+  function funcLogin() {
+    ${loginScript}
+  }
+  function funcCancel() {
+    const els = document
+      .getElementsByClassName("cm_time_list_tbl")[0]
+      .getElementsByTagName("tbody")[0]
+      .getElementsByTagName("tr");
+    let target;
+    Array.from(els).forEach((el) => {
+      const [btnTime, , btnDate, , , btnCourse] = el.children[3].children[0]
+        .getAttribute("onclick")
+        .inparen();
+      console.log("reserve cancel", course, date, time);
+      const fulldate = [year, month, date].join("");
+      if (btnDate == fulldate && btnCourse == course && btnTime == time)
+        target = el;
+    });
+    if (target) target.click();
+    const ac = window.AndroidController;
+    if (ac) ac.message("end of reserve/cancel");
+  }
+})();
--- script/reserve/reserve/360cc.js
+++ script/reserve/reserve/360cc.js
@@ -1,80 +1,81 @@
-javascript:(() => {
-    ${commonScript}
-    const addr = location.href;
-    const year = "${year}";
-    const month = "${month}";
-    const date = "${date}";
-    const course = "${course}";
-    const time = "${time}";
-    const dict = {
-        "${loginUrl}": funcLogin,
-        "${searchUrl}": funcReserve,
+javascript: (() => {
+  ${commonScript}
+  const addr = location.href;
+  const year = "${year}";
+  const month = "${month}";
+  const date = "${date}";
+  const course = "${course}";
+  const time = "${time}";
+  const dict = {
+    "${loginUrl}": funcLogin,
+    "${searchUrl}": funcReserve,
+  };
+  const func = dict[addr];
+  if (!func) location.href = "${searchUrl}";
+  else func();
+  function funcLogin() {
+    ${loginScript}
+  }
+  function funcReserve() {
+    const fulldate = [year, month, date].join("");
+    const dictCourse = { Out: "OUT", In: "IN" };
+    const signCourse = { Out: "1", In: "2" };
+    const param = {
+      type: "command",
+      sub_type: "reserve/reserve",
+      device_id: "${deviceId}",
+      device_token: "${deviceToken}",
+      golf_club_id: "a7fe6b1d-f05e-11ec-a93e-0242ac11000a",
+      message: "start reserve/reserve",
+      parameter: JSON.stringify({}),
     };
-    const func = dict[addr];    
-    if(!func) location.href = "${searchUrl}";
-    else func();
-    function funcLogin() {
-        ${loginScript}
-    };
-    function funcReserve() {
-        const fulldate = [year, month, date].join("");
-        const dictCourse = { Out: "OUT", In: "IN" };
-        const signCourse = { Out: "1", In: "2" };
+    TZLOG(param, (data) => {
+      log(data);
+    });
+    timefrom_change(fulldate, "2", "1", "", "00", "T");
+    const t = setInterval(() => {
+      if (tab0) {
+        timeapply_subcmd(
+          "R",
+          signCourse[course],
+          time,
+          "I",
+          "UNABLE",
+          "",
+          "N",
+          "N",
+          "",
+          "",
+          dictCourse[course],
+          "18홀",
+          "140,000",
+          "1"
+        );
+        clearInterval(t);
+        funcExec();
+      }
+    }, 1000);
+  }
+  function funcExec() {
+    const t = setInterval(() => {
+      if (agree_chk) {
+        agree_chk.checked = true;
+        golfsubcmd("R");
+        clearInterval(t);
         const param = {
-            type: "command",
-            sub_type: "reserve/reserve",
-            device_id: "${deviceId}",
-            device_token: "${deviceToken}",
-            golf_club_id: "a7fe6b1d-f05e-11ec-a93e-0242ac11000a",
-            message: "start reserve/reserve",
-            parameter: JSON.stringify({}),
-          };
-          TZLOG(param, (data) => {
-            log(data);
-          });
-        timefrom_change(fulldate, "2", "1", "", "00", "T");
-        const t = setInterval(() => {
-          if (tab0) {
-            timeapply_subcmd(
-              "R",
-              signCourse[course],
-              time,
-              "I",
-              "UNABLE",
-              "",
-              "N",
-              "N",
-              "",
-              "",
-              dictCourse[course],
-              "18홀",
-              "140,000",
-              "1"
-            );
-            clearInterval(t);
-            funcExec();
-          }
-        }, 1000);
-      };
-      function funcExec() {
-        const t = setInterval(() => {
-          if (agree_chk) {
-            agree_chk.checked = true;
-            golfsubcmd("R");
-            clearInterval(t);
-            const param = {
-                type: "command",
-                sub_type: "reserve/reserve",
-                device_id: "${deviceId}",
-                device_token: "${deviceToken}",
-                golf_club_id: "a7fe6b1d-f05e-11ec-a93e-0242ac11000a",
-                message: "end of reserve/reserve",
-                parameter: JSON.stringify({}),
-              };
-              TZLOG(param, (data) => {
-                log(data);
-              });
-          }
+          type: "command",
+          sub_type: "reserve/reserve",
+          device_id: "${deviceId}",
+          device_token: "${deviceToken}",
+          golf_club_id: "a7fe6b1d-f05e-11ec-a93e-0242ac11000a",
+          message: "end of reserve/reserve",
+          parameter: JSON.stringify({}),
+        };
+        TZLOG(param, (data) => {
+          const ac = window.AndroidController;
+          if (ac) ac.message("end of reserve/reserve");
         });
-      };
+      }
+    });
+  }
 })();
