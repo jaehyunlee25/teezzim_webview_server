8e14c1c6c5d5590a5273205c460fe8efd0d3c1a6
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-21 12:45:50 +0900

    'renew'

--- script/reserve/search/360cc.js
+++ script/reserve/search/360cc.js
@@ -1,55 +1,59 @@
-javascript:(() => {
-    ${commonScript}
-    const addr = location.href.split("?")[0];
-    const dict = {
-        "${loginUrl}": funcLogin,
-        "${reserveUrl}": funcReserve,
+javascript: (() => {
+  ${commonScript}
+  const addr = location.href.split("?")[0];
+  const dict = {
+    "${loginUrl}": funcLogin,
+    "${reserveUrl}": funcReserve,
+    "http://www.360cc.co.kr/mobile/main/mainPage.do": funcEnd,
+  };
+  const func = dict[addr];
+  if (func) func();
+  if (!func) location.href = "${reserveUrl}";
+  function funcLogin() {
+    ${loginScript}
+  }
+  function funcEnd() {
+    const ac = window.AndroidController;
+    if (ac) ac.message("end of reserve/search");
+  }
+  function funcReserve() {
+    const els = document
+      .getElementsByClassName("cm_time_list_tbl")[0]
+      .getElementsByTagName("tbody")[0]
+      .getElementsByTagName("tr");
+    const result = [];
+    const dictCourse = {
+      OUT: "Out",
+      IN: "IN",
     };
-    const func = dict[addr];    
-    if(func) func();
-    if(!func) location.href = "${reserveUrl}";
-    function funcLogin() {
-        ${loginScript}
-    };
-    function funcReserve() {
-        const els = document
-            .getElementsByClassName("cm_time_list_tbl")[0]
-            .getElementsByTagName("tbody")[0]
-            .getElementsByTagName("tr");
-        const result = [];
-        const dictCourse = {
-            OUT: "Out",
-            IN: "IN",
+    Array.from(els).forEach((el) => {
+      const [time, , date, , , course] = el.children[3].children[0]
+        .getAttribute("onclick")
+        .inparen();
+      console.log("reserve search", dictCourse[course], date, time);
+      result.push({ date, time, course: dictCourse[course] });
+      const param = {
+        golf_club_id: "${golfClubId}",
+        result,
+      };
+      const addr = OUTER_ADDR_HEADER + "/api/reservation/newReserveSearch";
+      post(addr, param, { "Content-Type": "application/json" }, (data) => {
+        console.log(data);
+        const param = {
+          type: "command",
+          sub_type: "reserve/search",
+          device_id: "${deviceId}",
+          device_token: "${deviceToken}",
+          golf_club_id: "a7fe6b1d-f05e-11ec-a93e-0242ac11000a",
+          message: "end of reserve/search",
+          parameter: JSON.stringify({}),
         };
-        Array.from(els).forEach((el) => {
-            const [time, , date, , , course] = el.children[3].children[0]
-                .getAttribute("onclick")
-                .inparen();
-            console.log("reserve search", dictCourse[course], date, time);
-            result.push({ date, time, course: dictCourse[course] });
-            const param = {
-                golf_club_id: "${golfClubId}",
-                result,
-            };
-            const addr = OUTER_ADDR_HEADER + "/api/reservation/newReserveSearch";
-            post(addr, param, { "Content-Type": "application/json" }, (data) => {
-                console.log(data);
-                const param = {
-                    type: "command",
-                    sub_type: "reserve/search",
-                    device_id: "${deviceId}",
-                    device_token: "${deviceToken}",
-                    golf_club_id: "a7fe6b1d-f05e-11ec-a93e-0242ac11000a",
-                    message: "end of reserve/search",
-                    parameter: JSON.stringify({}),
-                };
-                TZLOG(param, (data) => {
-                    log(data);
-                });
-                const ac = window.AndroidController;
-                if (ac) ac.message("end of reserve/search");
-                window["mm-m0-p0"].getElementsByTagName("a")[1].click();
-            });
+        TZLOG(param, (data) => {
+          log(data);
         });
-    };
+
+        window["mm-m0-p0"].getElementsByTagName("a")[1].click();
+      });
+    });
+  }
 })();
