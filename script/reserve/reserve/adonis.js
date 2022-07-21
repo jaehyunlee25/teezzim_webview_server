7e5fd460cf5d38d9ba8d86d0c5b356fd6a4697b5
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-21 23:59:00 +0900

    'renew'

---
+++ script/reserve/reserve/adonis.js
@@ -0,0 +1,58 @@
+javascript: (() => {
+  //${commonScript}
+  const addr = location.href.split("?")[0];
+  const year = "${year}";
+  const month = "${month}";
+  const date = "${date}";
+  const course = "${course}";
+  const time = "${time}";
+  const dict = {
+    "${loginUrl}": funcLogin,
+    "${searchUrl}": funcReserve,
+  };
+  const func = dict[addr];
+  if (!func) location.href = "${searchUrl}";
+  else func();
+  function funcLogin() {
+    //${loginScript}
+  }
+  function funcReserve() {
+    if (window["tab0"]) {
+      const els = tab0.getElementsByTagName("button");
+      const fulldate = [year, month, date].join("");
+      let target;
+      const dictCourse = {};
+      Array.from(els).forEach((el) => {
+        const param = el.getAttribute("onclick").inparen();
+        const signCourse = param[1].trim();
+        if (param[2].trim() == time && dictCourse[signCourse] == course)
+          target = el;
+      });
+      if (target) target.click();
+    } else if (document.getElementsByClassName("cm_btn default")[0]) {
+      const btn = document.getElementsByClassName("cm_btn default")[0];
+      if (btn) btn.click();
+    } else {
+      const param = {
+        type: "command",
+        sub_type: "reserve/reserve",
+        device_id: "${deviceId}",
+        device_token: "${deviceToken}",
+        golf_club_id: "${golfClubId}",
+        message: "start reserve/reserve",
+        parameter: JSON.stringify({}),
+      };
+      TZLOG(param, (data) => {
+        log(data);
+        const fulldate = [year, month, date].join("");
+        const els = document.getElementsByClassName("day-work");
+        let target;
+        els.forEach((el) => {
+          const eldate = el.getAttribute("date-date");
+          if (eldate == fulldate) target = el;
+        });
+        if (target) target.click();
+      });
+    }
+  }
+})();
