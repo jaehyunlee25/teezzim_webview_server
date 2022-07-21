f05fb79dcf15e144ec13efc632f99851817ae9c4
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-21 22:51:00 +0900

    'renew'

---
+++ script/reserve/search/adelscott.js
@@ -0,0 +1,17 @@
+javascript: (() => {
+  ${commonScript}
+  const addr = location.href.split("#")[0];
+  const dict = {
+    "${loginUrl}": funcLogin,
+    "${reserveUrl}": funcReserve,
+  };
+  const func = dict[addr];
+  if (!func) location.href = "${reserveUrl}";
+  else func();
+  function funcLogin() {
+    ${loginScript}
+  }
+  function funcReserve() {
+    log(11);
+  }  
+})();
