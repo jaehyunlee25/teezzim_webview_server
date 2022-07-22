03ff0b61ff8d0aca060ab7a51a46bcd6089cede9
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-22 19:11:09 +0900

    'renew'

--- operation.js
+++ operation.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const log = console.log;
 
-const clubFrom = "arista";
-const clubTo = "asecovalley";
+const clubFrom = "asecovalley";
+const clubTo = "baekje";
 
 ["reserve", "search", "cancel"].forEach((folder) => {
   const newName = "./script/reserve/" + folder + "/" + clubTo + ".js";
---
+++ script/reserve/cancel/baekje.js
@@ -0,0 +1,76 @@
+javascript: (() => {
+  ${commonScript}
+  const logParam = {
+    type: "command",
+    sub_type: "reserve/cancel",
+    device_id: "${deviceId}",
+    device_token: "${deviceToken}",
+    golf_club_id: "${golfClubId}",
+    message: "start reserve/cancel",
+    parameter: JSON.stringify({}),
+  };
+  const addr = location.href.split("?")[0];
+  const year = "${year}";
+  const month = "${month}";
+  const date = "${date}";
+  const course = "${course}";
+  const time = "${time}";
+  const dict = {
+    "${loginUrl}": funcLogin,
+    "${reserveUrl}": funcReserve,
+  };
+  const func = dict[addr];
+  if (!func) location.href = "${reserveUrl}";
+  else func();
+  function funcLogin() {
+    ${loginScript}
+  }
+  function funcReserve() {
+
+    const tag = localStorage.getItem("TZ_LOGOUT");
+    if (tag && new Date().getTime() - tag < 1000 * 5) return;
+    localStorage.setItem("TZ_LOGOUT", new Date().getTime());
+
+    TZLOG(logParam, (data) => {
+      log(data);
+      funcCancel();
+    });
+  }
+  function funcCancel() {
+    const els = document.getElementsByClassName("cm_cnlth");
+    const dictCourse = {
+      단일: "단일",
+    };
+    let target;
+    Array.from(els).forEach((el) => {
+      const param = el.children[0].getAttribute("href").inparen();
+
+      const elDate = param[2];
+      const elTime = param[3];
+      const elCourse = "단일";
+      console.log("reserve cancel", dictCourse[elCourse], elDate, elTime);
+      const fulldate = [year, month, date].join("");
+      if (
+        elDate == fulldate &&
+        dictCourse[elCourse] == course &&
+        elTime == time
+      )
+        target = el.children[0];
+    });
+    if (target) {
+      target.click();
+      setTimeout(funcEnd, 1000);
+    } else {
+      funcEnd();
+    }
+  }
+  function funcEnd() {
+    const strEnd = "end of reserve/cancel";
+    logParam.message = strEnd;
+    TZLOG(logParam, (data) => {
+      const ac = window.AndroidController;
+      if (ac) ac.message(strEnd);
+      location.href = "/_mobile/login/logout.asp";
+    });
+  }
+})();
