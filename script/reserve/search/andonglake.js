fbcec06739c02d859d1c6684257dad1359384315
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-22 13:53:51 +0900

    'renew'

--- script/reserve/search/andonglake.js
+++ script/reserve/search/andonglake.js
@@ -23,8 +23,8 @@ javascript: (() => {
   function funcReserve() {
     const tag = localStorage.getItem("TZ_RESERVE") * 1;    
     if(tag && (new Date().getTime() - tag) < 1000 * 5) return;
+    localStorage.setItem("TZ_RESERVE", new Date().getTime());
     TZLOG(logParam, (data) => {
-      localStorage.setItem("TZ_RESERVE", new Date().getTime());
       funcSearch();
     });
   }
