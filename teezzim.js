d22f2e1be746d32423d2f86acf46547b14c15f0c
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-23 19:28:50 +0900

    'renew'

--- teezzim.js
+++ teezzim.js
@@ -370,6 +370,35 @@ function procPost(request, response, data) {
     const commonScript = fs.readFileSync("script/search/common.js", "utf-8");
     const loginUrl = golfClubLoginUrl[engName];
     const reserveUrl = golfClubReserveUrl[engName];
+    const loginScript = getPureLoginScript(engName);
+    let templateScript;
+    if (fs.existsSync("script/reserve/search/" + engName + ".js"))
+      templateScript = fs.readFileSync(
+        "script/reserve/search/" + engName + ".js",
+        "utf-8"
+      );
+    else
+      templateScript = fs.readFileSync(
+        "script/reserve/search/reserveSearchTemplate.js",
+        "utf-8"
+      );
+    const golfClubId = golfClubIds[engName];
+    const script = templateScript.dp({
+      golfClubId,
+      commonScript,
+      loginUrl,
+      reserveUrl,
+      loginScript,
+    });
+    objResp = {
+      url: loginUrl,
+      script,
+    };
+  } else if (request.url == "/reserveSearchbot_admin") {
+    const { club: engName, year, month, date, course, time } = data;
+    const commonScript = fs.readFileSync("script/search/common.js", "utf-8");
+    const loginUrl = golfClubLoginUrl[engName];
+    const reserveUrl = golfClubReserveUrl[engName];
     const loginScript = getPureLoginScript(engName).dp({
       login_id: golfClubAccounts[engName].id,
       login_password: golfClubAccounts[engName].pw,
