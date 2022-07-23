51daf1c0980191e322d2c3de8ef3f82ba43ed967
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-23 21:23:39 +0900

    'renew'

--- script/reserve/reserve/eodeungsan.js
+++ script/reserve/reserve/eodeungsan.js
@@ -38,7 +38,7 @@ javascript: (() => {
     Book_Confirm(fulldate, "", dictCourse[course], course, time, "2");
   }
   function funcExec() {
-    document.getElementsByClassName("btn_reserve").children[0].click();
+    document.getElementsByClassName("btn_reserve")[0].children[0].click();
     setTimeout(() => {
       const ac = window.AndroidController;
       if (ac) ac.message("end of reserve/reserve");
