ed7edcd2c6ce6451fac185336751cbc7e1e79781
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-23 17:53:29 +0900

    'renew'

--- script/reserve/cancel/centerium.js
+++ script/reserve/cancel/centerium.js
@@ -9,6 +9,7 @@ javascript: (() => {
     message: "start reserve/cancel",
     parameter: JSON.stringify({}),
   };
+  log("raw addr :: ", location.href);
   const addr = location.href.split("?")[0];
   const year = "${year}";
   const month = "${month}";
@@ -19,10 +20,12 @@ javascript: (() => {
     "${loginUrl}": funcLogin,
     "${reserveUrl}": funcReserve,
   };
+  log("addr", addr);
   const func = dict[addr];
   if (!func) location.href = "${reserveUrl}";
   else func();
-  function funcLogin() {    
+  function funcLogin() {  
+    log("funcLogin");  
     const tag = localStorage.getItem("TZ_LOGOUT");
     if (tag && new Date().getTime() - tag < 1000 * 10) return;
     localStorage.setItem("TZ_LOGOUT", new Date().getTime());
@@ -30,6 +33,7 @@ javascript: (() => {
     ${loginScript}
   }
   function funcReserve() {
+    log("funcReserve");
     const tag = localStorage.getItem("TZ_RESERVE");
     if (tag && new Date().getTime() - tag < 1000 * 5) {
       funcEnd();
@@ -43,6 +47,7 @@ javascript: (() => {
     });
   }
   function funcCancel() {
+    log("funcCancel");
     const els = document.getElementsByClassName("courseTime");
     const dictCourse = {
       A: "England",
@@ -52,13 +57,21 @@ javascript: (() => {
     let target;
     Array.from(els).forEach((el) => {
       const param = el.innerText;
-      const elDate = "20" + /[0-9]{2}\/[0-9]{2}/.exec(param)[0].split("/").join("");
+      let elDate = /[0-9]{2}\/[0-9]{2}/.exec(param)[0].split("/").join("");
+      
+      let year = new Date().getFullYear();
+      const nMonth = new Date().getMonth() + 1;
+      const cMonth = elDate.gh(2) * 1;
+      if(cMonth < nMonth) year += 1;
+      
+      elDate = year.toString() + elDate;     
+      
       const elTime = /[0-9]{2}\:[0-9]{2}/.exec(param)[0].split(":").join("");
       const elCourse = /[A-Za-z]+/.exec(param)[0];
       console.log("reserve search", elCourse, elDate, elTime);
       const fulldate = [year, month, date].join("");
       log(elDate, fulldate,
-        dictCourse[elCourse], course,
+        elCourse, course,
         elTime, time);
       if (
         elDate == fulldate &&
@@ -76,6 +89,7 @@ javascript: (() => {
     } */
   }
   function funcEnd() {
+    log("funcEnd");
     const strEnd = "end of reserve/cancel";
     logParam.message = strEnd;
     TZLOG(logParam, (data) => {
