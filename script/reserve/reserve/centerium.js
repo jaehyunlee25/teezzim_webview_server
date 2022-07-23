31ba34b53bcffa288ba87a9e76377f904dbd60b8
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-23 16:45:55 +0900

    'renew'

--- script/reserve/reserve/centerium.js
+++ script/reserve/reserve/centerium.js
@@ -95,8 +95,10 @@ javascript: (() => {
   function funcExec() {
     log("funcExec");
     document.getElementsByClassName("darkRedBtn")[0].children[0].click();
-    chkSubmit_bk1();
-    setTimeout(funcEnd, 1000);
+    setTimeout(()=> {
+      chkSubmit_bk1();
+      setTimeout(funcEnd, 1000);
+    }, 500);
   }
   function funcEnd() {
     log("funcEnd");
