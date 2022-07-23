adeaa492855cc06f5decef599829bf96054b9d30
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-23 12:39:00 +0900

    'renew'

--- script/reserve/reserve/boryeongbase.js
+++ script/reserve/reserve/boryeongbase.js
@@ -32,6 +32,12 @@ javascript: (() => {
     ${loginScript}
   }
   function funcReserve() {
+    
+    const tag = localStorage.getItem("TZ_LOGOUT");
+    if (tag && new Date().getTime() - tag < 1000 * 5) return;
+    localStorage.setItem("TZ_LOGOUT", new Date().getTime());
+    
+    
     log("enter addr", addr);
     log("enter suffix", suffix);
     if(suffix) {
@@ -39,9 +45,6 @@ javascript: (() => {
       return;
     }
 
-    const tag = localStorage.getItem("TZ_LOGOUT");
-    if (tag && new Date().getTime() - tag < 1000 * 5) return;
-    localStorage.setItem("TZ_LOGOUT", new Date().getTime());
 
     TZLOG(logParam, (data) => {
       const first = [year, month, "01"].join("-");
