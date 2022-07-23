3369a328bff3d4e4ceda89cebb37d7a3ff0d18c7
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-23 14:59:46 +0900

    'renew'

--- script/reserve/reserve/bosung.js
+++ script/reserve/reserve/bosung.js
@@ -34,7 +34,11 @@ javascript: (() => {
   else func();
 
   function funcMain() {
-    return;
+    const tag = localStorage.getItem("TZ_MAIN");
+    if (tag && new Date().getTime() - tag < 1000 * 5) return;
+    localStorage.setItem("TZ_MAIN", new Date().getTime());
+    
+    location.href = "${searchUrl}";
   }
   function funcLogin() {
     ${loginScript}
