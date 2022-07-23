d549ac3d83e25a7a5e69ca6341c9e1fbdd3d8fa0
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-23 17:55:36 +0900

    'renew'

--- script/reserve/cancel/centerium.js
+++ script/reserve/cancel/centerium.js
@@ -95,7 +95,7 @@ javascript: (() => {
     TZLOG(logParam, (data) => {
       const ac = window.AndroidController;
       if (ac) ac.message(strEnd);
-      // location.href = "/_mobile/login/logout.asp";
+      /* location.href = "/_mobile/login/logout.asp"; */
     });
   }
 })();
