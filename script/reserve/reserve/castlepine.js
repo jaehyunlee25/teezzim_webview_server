99caad828bfcc71c6133472f10f3a9d371a548cd
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-23 15:36:26 +0900

    'renew'

--- script/reserve/reserve/castlepine.js
+++ script/reserve/reserve/castlepine.js
@@ -60,6 +60,7 @@ javascript: (() => {
       return result;
     })();
 
+    log("settype", suffixParam["settype"]);
     if (suffixParam["settype"] == "T") {
       log("calendar");
       funcDate();
