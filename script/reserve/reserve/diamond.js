4d2c32e13733bd96ef3b66365dfd916f5bf8cd65
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-24 18:27:13 +0900

    'renew'

--- script/reserve/reserve/diamond.js
+++ script/reserve/reserve/diamond.js
@@ -93,6 +93,11 @@ javascript: (() => {
     });
   }
   function funcTime() {
+
+    const tag = localStorage.getItem("TZ_TIME");
+    if (tag && new Date().getTime() - tag < 1000 * 5) return;
+    localStorage.setItem("TZ_TIME", new Date().getTime());
+
     log("funcTime");
     setTimeout(() => {
       const sign = dictCourse[course];
