3224de6624b431010e49e5dbef4892b20e1400f7
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-24 18:09:47 +0900

    'renew'

--- operation.js
+++ operation.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const log = console.log;
 
 const clubFrom = "daegaya";
-const clubTo = "delphino";
+const clubTo = "diamond";
 
 ["reserve", "search", "cancel"].forEach((folder) => {
   const newName = "./script/reserve/" + folder + "/" + clubTo + ".js";
