5892e8c3d66603bb45eee10d81f15af20adba63f
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-24 21:10:56 +0900

    'renew'

--- script/reserve/cancel/dongwongolf.js
+++ script/reserve/cancel/dongwongolf.js
@@ -27,6 +27,14 @@ javascript: (() => {
   if (!func) funcOther();
   else func();
 
+  function funcMain() {
+    log("funcMain");
+    const tag = localStorage.getItem("TZ_MAIN");
+    if (tag && new Date().getTime() - tag < 1000 * 5) return;
+    localStorage.setItem("TZ_MAIN", new Date().getTime());
+
+    location.href = "${reserveUrl}";
+  }
   function funcOut() {
     log("funcOut");
     return;
