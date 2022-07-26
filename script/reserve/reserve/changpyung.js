52a1ca364c8147fb4ee49a88290b7d5ec359bdc8
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-26 23:13:49 +0900

    'renew'

--- script/reserve/reserve/changpyung.js
+++ script/reserve/reserve/changpyung.js
@@ -23,7 +23,7 @@ javascript: (() => {
   const dict = {
     "${loginUrl}": funcLogin,
     "${searchUrl}": funcReserve,
-    "https://m.cppc.co.kr/index.asp": funcMain,
+    "https://m.cppc.co.kr/_html/reserve_info.asp": funcMain,
     "https://m.cppc.co.kr/_html/member/logout_ok.asp": funcEnd,
     "https://m.cppc.co.kr/_html/reserve_time.part.asp": funcTime,
     "https://m.cppc.co.kr/_html/reserve_form.asp": funcExec,
