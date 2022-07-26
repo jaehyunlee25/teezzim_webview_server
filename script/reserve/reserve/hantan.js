6b879886a069afa189c0a24797f4c16bf4609347
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-26 21:11:57 +0900

    'renew'

--- script/reserve/reserve/hantan.js
+++ script/reserve/reserve/hantan.js
@@ -51,7 +51,6 @@ javascript: (() => {
   }
   function funcMain() {
     log("funcMain");
-    return;
     const tag = localStorage.getItem("TZ_MAIN");
     if (tag && new Date().getTime() - tag < 1000 * 5) return;    
     localStorage.setItem("TZ_MAIN", new Date().getTime());
