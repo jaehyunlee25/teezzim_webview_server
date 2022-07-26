bb648e8c1fda550395133ba4ef12abbddfef472a
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-27 03:02:04 +0900

    'renew'

--- script/reserve/cancel/bomun.js
+++ script/reserve/cancel/bomun.js
@@ -35,7 +35,10 @@ javascript: (() => {
     log("funcReserve");
     
     const tag = localStorage.getItem("TZ_RESERVE") * 1;    
-    if(tag && (new Date().getTime() - tag) < 1000 * 5) return;
+    if(tag && (new Date().getTime() - tag) < 1000 * 5) {
+      funcEnd();
+      return;
+    }
     localStorage.setItem("TZ_RESERVE", new Date().getTime());
 
     TZLOG(logParam, (data) => {
@@ -50,14 +53,13 @@ javascript: (() => {
     };
     const fd = [year.ch(2), month, date].join("");
     const key = [fd, time, dictCourse[course]].join("");
-    
+
     log("key", key);
     let target = window[key];    
     log(target);
     if (target) {
       target.click();
       btn_delete.click();
-      setTimeout(funcEnd, 1000);
     } else {
       funcEnd();
     }
