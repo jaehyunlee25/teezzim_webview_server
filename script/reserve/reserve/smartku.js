c0db37363aa0804cf4bc5884c68bb067f2734083
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-28 02:05:09 +0900

    'renew'

--- script/reserve/reserve/smartku.js
+++ script/reserve/reserve/smartku.js
@@ -67,15 +67,24 @@ javascript: (() => {
     if (tag && new Date().getTime() - tag < 1000 * 5) return;
     localStorage.setItem("TZ_MAIN", new Date().getTime());
 
-    log(addr);
-    /* location.href = "${searchUrl}"; */
+    if(addr.indexOf("returnurl") != -1) {
+        lss("TZ_RETURN", "true");
+        funcLogin();
+    } else {
+      location.href = "${searchUrl}";
+    }
   }
   function funcLogin() {
     log("funcLogin");
 
-    const tag = localStorage.getItem("TZ_LOGIN");
-    if (tag && new Date().getTime() - tag < 1000 * 5) return;
-    localStorage.setItem("TZ_LOGIN", new Date().getTime());
+    if(lsg("TZ_RETURN") == "true") {
+      log("TZ_RETURN");
+      lsr("TZ_RETURN");
+    } else {
+      const tag = localStorage.getItem("TZ_LOGIN");
+      if (tag && new Date().getTime() - tag < 1000 * 5) return;
+      localStorage.setItem("TZ_LOGIN", new Date().getTime());
+    }
 
     ${loginScript}
   }
