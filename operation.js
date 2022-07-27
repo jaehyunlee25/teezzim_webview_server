b58af2ae6699ede10734a35b4408ae730e3787aa
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-28 03:57:14 +0900

    'renew'

--- operation.js
+++ operation.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const log = console.log;
 
-const clubFrom = "jayuro";
-const clubTo = "parkvalley";
+const clubFrom = "stonegate";
+const clubTo = "yongin";
 
 ["reserve", "search", "cancel"].forEach((folder) => {
   const newName = "./script/reserve/" + folder + "/" + clubTo + ".js";
---
+++ script/reserve/search/yongin.js
@@ -0,0 +1,109 @@
+javascript: (() => {
+  ${commonScript}
+  const logParam = {
+    type: "command",
+    sub_type: "reserve/search",
+    device_id: "${deviceId}",
+    device_token: "${deviceToken}",
+    golf_club_id: "${golfClubId}",
+    message: "start reserve/search",
+    parameter: JSON.stringify({}),
+  };
+  const addr = location.href.split("?")[0];
+  const dict = {
+    "${loginUrl}": funcLogin,
+    "${reserveUrl}": funcReserve,
+    "https://www.stonegatecc.co.kr/Mobile/": funcMain,
+    "https://www.stonegatecc.co.kr/Mobile/Member/LogOut.aspx": funcOut,
+  };
+  
+  log("raw addr :: ", location.href);
+  log("addr :: ", addr);
+
+  const func = dict[addr];
+  if (!func) funcOther();
+  else func();
+
+  function funcMain() {
+    log("funcMain");
+    const tag = localStorage.getItem("TZ_MAIN");
+    if (tag && new Date().getTime() - tag < 1000 * 10) {
+      funcEnd();
+      return;
+    }
+    localStorage.setItem("TZ_MAIN", new Date().getTime());
+
+    location.href = "${reserveUrl}";
+  }
+  function funcOut() {
+    log("funcOut");
+    funcEnd();
+    return;
+  }
+  function funcOther() {
+    log("funcOther");
+    const tag = localStorage.getItem("TZ_MAIN");
+    if (tag && new Date().getTime() - tag < 1000 * 10) return;
+    localStorage.setItem("TZ_MAIN", new Date().getTime());
+
+    location.href = "${reserveUrl}";
+  }
+  function funcLogin() {
+    log("funcLogin");
+
+    const tag = localStorage.getItem("TZ_LOGIN");
+    if (tag && new Date().getTime() - tag < 1000 * 10) return;
+    localStorage.setItem("TZ_LOGIN", new Date().getTime());
+
+    ${loginScript}
+  }
+  function funcReserve() {
+    log("funcReserve");
+
+    const tag = localStorage.getItem("TZ_RESERVE");
+    if (tag && new Date().getTime() - tag < 1000 * 10) return;
+    localStorage.setItem("TZ_RESERVE", new Date().getTime());
+
+    TZLOG(logParam, (data) => {});
+    funcSearch();
+  }
+  function funcSearch() {
+    log("funcSearch");
+
+    const els = doc.gcn("cancelBtn");
+    const dictCourse = {
+      11: "스톤",
+      22: "게이트",
+    };
+    const result = [];
+    Array.from(els).forEach((el) => {
+      const param = el.attr("href").inparen();
+      const [date, time, course] = param;
+      
+      log("reserve search", dictCourse[course], date, time);
+      result.push({ date, time, course: dictCourse[course] });
+    });
+    const param = {
+      golf_club_id: "${golfClubId}",
+      device_id: "${deviceId}",
+      result,
+    };
+    const addr = OUTER_ADDR_HEADER + "/api/reservation/newReserveSearch";
+    post(addr, param, { "Content-Type": "application/json" }, (data) => {
+      console.log(data);
+      LOGOUT();
+    });
+  }
+  function funcEnd() {
+    log("funcEnd");
+    const strEnd = "end of reserve/search";
+    logParam.message = strEnd;
+    TZLOG(logParam, (data) => {});
+    const ac = window.AndroidController;
+    if (ac) ac.message(strEnd);
+  }
+  function LOGOUT() {
+    log("LOGOUT");
+    location.href = "/Mobile/Member/LogOut.aspx";
+  }
+})();
