615a071f349a28bc5a67b66348917bf449bfd8a9
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-28 16:54:28 +0900

    'renew'

--- script/reserve/search/adelscott.js
+++ script/reserve/search/adelscott.js
@@ -98,7 +98,7 @@ javascript: (() => {
   }
   function funcEnd() {
     log("funcEnd");
-    const strEnd = "end of reserve/reserve";
+    const strEnd = "end of reserve/search";
     logParam.message = strEnd;
     TZLOG(logParam, (data) => {});
     const ac = window.AndroidController;
