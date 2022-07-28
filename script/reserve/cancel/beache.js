44107d86f4ef447fb70bccd6d0f924c66e13bf6e
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-28 19:48:49 +0900

    'renew'

--- script/reserve/cancel/beache.js
+++ script/reserve/cancel/beache.js
@@ -66,7 +66,10 @@ javascript: (() => {
     log("funcReserve");
     
     const tag = localStorage.getItem("TZ_RESERVE");
-    if (tag && new Date().getTime() - tag < 1000 * 5) return;
+    if (tag && new Date().getTime() - tag < 1000 * 5) {
+      LOGOUT();
+      return;
+    }
     localStorage.setItem("TZ_RESERVE", new Date().getTime());
 
     TZLOG(logParam, (data) => {
