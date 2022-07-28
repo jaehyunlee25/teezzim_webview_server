288d90a3b9695d16798483485f53648aeecef30b
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-28 18:33:30 +0900

    'renew'

--- script/reserve/search/andonglake.js
+++ script/reserve/search/andonglake.js
@@ -95,7 +95,7 @@ javascript: (() => {
   }
   function funcEnd() {
     log("funcEnd");
-    const strEnd = "end of reserve/reserve";
+    const strEnd = "end of reserve/search";
     logParam.message = strEnd;
     TZLOG(logParam, (data) => {});
     const ac = window.AndroidController;
