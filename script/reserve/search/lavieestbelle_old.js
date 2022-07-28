410a0bb455341934282fa45f47b99818f3846b89
jaehyunlee <jaehyunlee25@gmail.com>
2022-07-28 21:21:21 +0900

    'renew'

--- script/reserve/search/lavieestbelle_old.js
+++ script/reserve/search/lavieestbelle_old.js
@@ -82,7 +82,7 @@ javascript: (() => {
     };
     const result = [];
     Array.from(els).forEach((el) => {
-      const [ , , , date, time, course] = el.attr("href").inparen();
+      const [ , , , date, time, course] = el.children[0].attr("href").inparen();
   
       log("reserve search", dictCourse[course], date, time);
       result.push({ date, time, course: dictCourse[course] });
