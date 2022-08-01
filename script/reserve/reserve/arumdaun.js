b6d49be8cdaf07fe4947dc02a109f52bc732d236
jaehyunlee <jaehyunlee25@gmail.com>
2022-08-01 14:31:58 +0900

    'renew'

--- script/reserve/reserve/arumdaun.js
+++ script/reserve/reserve/arumdaun.js
@@ -24,6 +24,7 @@ javascript: (() => {
     "http://m2.arumdaunresort.com/main.asp": funcMain,
     "http://m2.arumdaunresort.com/reserve_confirm1.asp": funcList,
     "http://m2.arumdaunresort.com/reserve_02_Book.asp": funcTime,
+    "http://m2.arumdaunresort.com/reserve_02_book.asp": funcExec,
   };
   
   log("raw addr :: ", location.href);
@@ -84,40 +85,35 @@ javascript: (() => {
   function funcTime() {
     log("funcTime");
 
-    if (!suffix) {
-      const els = doc.gcn("btn02");
-      
-      let target;
-      Array.from(els).every((el) => {
-        const param = el.attr("href").inparen();
-        const elCourse = param[2];
-        const elTime = param[4];
+    const els = doc.gcn("btn02");
+    
+    let target;
+    Array.from(els).every((el) => {
+      const param = el.attr("href").inparen();
+      const elCourse = param[2];
+      const elTime = param[4];
 
-        log(dictCourse[course] == elCourse, time == elTime);
-        log(dictCourse[course], elCourse, time, elTime);
-        if (dictCourse[course] == elCourse && time == elTime) target = el;
+      log(dictCourse[course] == elCourse, time == elTime);
+      log(dictCourse[course], elCourse, time, elTime);
+      if (dictCourse[course] == elCourse && time == elTime) target = el;
 
-        return !target;
-      });
+      return !target;
+    });
 
-      log("target", target);
-      if (target) {
-        target.click();
-      } else {
-        LOGOUT()
-      }
-      
+    log("target", target);
+    if (target) {
+      target.click();
     } else {
-      funcExec();
-    }   
+      LOGOUT()
+    }
   }
   function funcExec() {
     log("funcExec");
 
     const strEnd = "end of reserve/reserve";
-    const cfrmNo = document.getElementsByName("apply_no")[0].value;
-    document.getElementsByName("apply_no_re")[0].value = cfrmNo;
-    document.getElementsByClassName("btn002")[0].click();
+    const cfrmNo = doc.getElementsByName("apply_no")[0].value;
+    doc.getElementsByName("apply_no_re")[0].value = cfrmNo;
+    doc.gcn("btn002")[0].click();
     
   }
   function funcEnd() {
