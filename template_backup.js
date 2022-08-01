0c6aa8fde70f60c99f3b0849b8ff8d26f275c10a
jaehyunlee <jaehyunlee25@gmail.com>
2022-08-01 18:17:45 +0900

    'renew'

---
+++ template_backup.js
@@ -0,0 +1,11 @@
+javascript:(() => {
+    ${commonScript}
+    const addr = location.href;
+    if(addr == "${loginUrl}") {
+        ${loginScript}
+    } else if (addr == "${searchUrl}") {
+        ${searchScript}
+    } else {
+        location.href = "${searchUrl}";
+    }
+})();
