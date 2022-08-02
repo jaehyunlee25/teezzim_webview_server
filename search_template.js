b277b4ff6bc71591ed211baef87d630f9ee53d99
jaehyunlee <jaehyunlee25@gmail.com>
2022-08-02 12:49:51 +0900

    'renew'

--- search_template.js
+++ search_template.js
@@ -5,7 +5,8 @@ if(lsg("TZ_ADMIN_BLOCK_IC")) {
 	const lastVistTime = lsg("TZ_ADMIN_BLOCK_IC_TIME") * 1;
 	if (lastVistTime < 1000 * 15) {
 		if (visitNumber > 9) {
-			window.AndroidController.message("TZ_MSG_IC");
+			if(window.AndroidController) 
+				window.AndroidController.message("TZ_MSG_IC");
 			TZ_BOT_SAFETY = false;
 			/* 초기화 */
 			lss("TZ_ADMIN_BLOCK_IC", 0);
