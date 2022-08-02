7ece2ef781fc914080750eb539a071cfc68badc0
jaehyunlee <jaehyunlee25@gmail.com>
2022-08-02 12:55:22 +0900

    'renew'

--- template.js
+++ template.js
@@ -2,6 +2,13 @@ javascript:(() => {
     ${commonScript}
     const addr = location.href;
     if(addr == "${loginUrl}") {
+        const tag = localStorage.getItem("TZ_LOGIN");
+        if (tag && new Date().getTime() - tag < 1000 * 10) {
+            location.href = "${searchUrl}";
+            return;
+        }
+        localStorage.setItem("TZ_LOGIN", new Date().getTime());
+
         ${loginScript}
     } else if (addr == "${searchUrl}") {
         ${searchScript}
