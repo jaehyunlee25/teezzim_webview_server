74507bfd82e588adb3b0bb82a4a498294452f32a
jaehyunlee <jaehyunlee25@gmail.com>
2022-08-02 16:17:59 +0900

    'renew'

--- search_template.js
+++ search_template.js
@@ -13,6 +13,9 @@ if(lsg("TZ_ADMIN_BLOCK_IC")) {
 			lss("TZ_ADMIN_BLOCK_IC_TIME", new Date().getTime());
 			/* 로그아웃 */
 			if (LOGOUT) LOGOUT();
+		} else {
+			visitNumber++;
+			lss("TZ_ADMIN_BLOCK_IC", visitNumber);
 		}
 	} else {
 		lss("TZ_ADMIN_BLOCK_IC", 0);
@@ -24,6 +27,18 @@ if(lsg("TZ_ADMIN_BLOCK_IC")) {
 }
 log("TZ_ADMIN_BLOCK_IC", lsg("TZ_ADMIN_BLOCK_IC"), lsg("TZ_ADMIN_BLOCK_IC_TIME"));
 /* end blocking infinite call */
+
+const splitter = location.href.indexOf("?") == -1 ? "#" : "?";
+const aDDr = location.href.split(splitter)[0];
+const suffix = location.href.split(splitter)[1];
+const dictSplitter = {"#": "?", "?": "#"};
+let addr = aDDr;
+if(aDDr.indexOf(dictSplitter[splitter]) != -1) 
+    addr = aDDr.split(dictSplitter[splitter])[0];
+
+log("raw addr :: ", location.href);
+log("aDDr :: ", aDDr);
+log("addr :: ", addr);
 
 const clubId = '${golf_club_id}';
 const courses = { 
