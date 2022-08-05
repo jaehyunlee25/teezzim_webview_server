208dcecd93757839a6a646fdcf6dc87d434951cb
jaehyunlee <jaehyunlee25@gmail.com>
2022-08-05 19:32:11 +0900

    'renew'

--- operation.js
+++ operation.js
@@ -14,7 +14,7 @@ files.forEach((file) => {
   const sec = time.ct(3);
   if (!clubs[club][sec]) clubs[club][sec] = [];
   clubs[club][sec].push([
-    file,
+    "./script/backup/" + file,
     ["./script/reserve_core", two, club, type + ".json"].join("/"),
   ]);
 });
@@ -24,12 +24,16 @@ Object.keys(clubs).forEach((key) => {
   const club = clubs[key];
   const dates = Object.keys(club);
   dates.sort((a, b) => b - a);
-  log(dates);
-  curClub[key] = club[dates[0]];
+  if (!curClub[key]) curClub[key] = {};
+  curClub[key][dates[0]] = club[dates[0]];
 });
+
 Object.keys(curClub).forEach((key) => {
-  const club = clubs[key];
-  const time = Object.keys(club)[0];
-  //log(time);
-  // log(club);
+  const club = curClub[key];
+  const date = Object.keys(club)[0];
+  const files = club[date];
+  files.forEach((file) => {
+    const con = fs.readFileSync(file[0], "utf-8");
+    fs.writeFileSync(file[1], con, "utf-8");
+  });
 });
