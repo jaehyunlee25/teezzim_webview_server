2cfa0152a00752e98cd4e9bdd8b2f70adcee880b
jaehyunlee <jaehyunlee25@gmail.com>
2022-08-09 16:49:05 +0900

    'renew'

--- teezzim.js
+++ teezzim.js
@@ -367,7 +367,7 @@ function procPost(request, response, data) {
     const ids = {};
     const command = "GET_DATE";
     clubs.forEach((club) => {
-      const result = searchbotDate({ club, command });
+      const result = searchbot({ club, command });
       urls[club] = result.url;
       scripts[club] = result.script;
       ids[club] = golfClubIds[club];        
