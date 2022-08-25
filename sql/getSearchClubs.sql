883ab96397bb00998a6da13441523b289f71841b
jaehyunlee <jaehyunlee25@gmail.com>
2022-08-25 13:03:05 +0900

    'renew'

--- sql/getSearchClubs.sql
+++ sql/getSearchClubs.sql
@@ -6,5 +6,5 @@ FROM golf_club
 JOIN golf_club_detail ON golf_club.id = golf_club_detail.golf_club_id
 JOIN golf_club_eng on golf_club.id = golf_club_eng.golf_club_id
 JOIN golf_club_usability ON golf_club.id = golf_club_usability.golf_club_id
-WHERE golf_club_detail.reserve_script = TRUE
-ORDER BY golf_club_detail.reserve_script_date asc;
+WHERE golf_club_detail.search_script = TRUE
+ORDER BY golf_club_detail.search_script_date asc;
