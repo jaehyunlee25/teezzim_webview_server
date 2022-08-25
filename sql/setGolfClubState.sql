6e677458a47eda3dc541caf92a1735b45a0ca929
jaehyunlee <jaehyunlee25@gmail.com>
2022-08-25 13:42:14 +0900

    'renew'

--- sql/setGolfClubState.sql
+++ sql/setGolfClubState.sql
@@ -1,6 +1,7 @@
 update
     golf_club_usability 
 set 
-    golf_club_state = ${golfClubState}
+    golf_club_state = ${golfClubState},
+    updated_at = NOW()
 where
     golf_club_id = '${golfClubId}';
