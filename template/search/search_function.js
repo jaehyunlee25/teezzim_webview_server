59c589f6f70611c3e592b0104df5775f2b8118e1
jaehyunlee <jaehyunlee25@gmail.com>
2022-08-30 12:32:02 +0900

    'renew'

--- template/search/search_function.js
+++ template/search/search_function.js
@@ -89,20 +89,19 @@ function procGolfSchedule() {
       obj.time = obj.time.gh(2) + ":" + obj.time.gt(2);
   });
   /* console.log(golf_schedule); */
-  if (golf_schedule.length == 0) {
-    log("예약가능한 시간이 없습니다.");
-    if (ac) ac.message("NONE_OF_GET_SCHEDULE");
-    LOGOUT();
-    return;
-  }
   const param = { golf_schedule, golf_club_id: clubId };
   post(addrOuter, param, header, (data) => {
     const json = JSON.parse(data);
     log(json.message);
-    if (json.resultCode == 200) {
-      if (ac) ac.message("end of procGolfSchedule!");
-    } else {
-      if (ac) ac.message("FAIL_OF_GET_SCHEDULE");
+    if (golf_schedule.length == 0) {
+      log("예약가능한 시간이 없습니다.");
+      if (ac) ac.message("NONE_OF_GET_SCHEDULE");
+    } else { 
+      if (json.resultCode == 200) {
+        if (ac) ac.message("end of procGolfSchedule!");
+      } else {
+        if (ac) ac.message("FAIL_OF_GET_SCHEDULE");
+      }
     }
     LOGOUT();
   });
