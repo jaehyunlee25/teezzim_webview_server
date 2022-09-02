dd703ed2d953ea0566569e69964685fcfbe80723
jaehyunlee <jaehyunlee25@gmail.com>
2022-09-02 13:25:11 +0900

    'renew'

--- teezzim.js
+++ teezzim.js
@@ -181,9 +181,9 @@ function procPost(request, response, data) {
     objResp = 0;
   } else if (request.url == "/delDeviceRecord") {
     delDeviceDate(data, (res1) => {
-      log(res1);
+      dir(res1);
       delDeviceTime(data, (res2) => { 
-        log(res2);
+        dir(res2);
         objResp = {
           resultCode: 200,
           message: "디바이스 자료를 모두 삭제했습니다.",
