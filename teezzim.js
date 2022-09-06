f12497a5edb2b9ef5521dd1f1ea0afcdef1edc36
jaehyunlee <jaehyunlee25@gmail.com>
2022-09-06 14:18:54 +0900

    'renew'

--- teezzim.js
+++ teezzim.js
@@ -14,7 +14,48 @@ const log = function () {
   console.log("\n\n>> new log :: ", new Date());
   console.log(Array.from(arguments).join(", "));
 };
+
 const dir = console.dir;
+String.prototype.dp = function (param) {
+  let self = this;
+  const keys = Object.keys(param);
+  keys.forEach((key) => {
+    const regex = new RegExp("\\$\\{".add(key).add("\\}"), "g");
+    const val = param[key];
+    self = self.replace(regex, val);
+  });
+  return self;
+};
+String.prototype.add = function add(str) {
+  return [this, str].join("");
+};
+String.prototype.jp = function () {
+  return JSON.parse(this);
+};
+String.prototype.gf = function () {
+  const path = this.toString();
+  return fs.readFileSync(path, "utf-8");
+};
+String.prototype.gfjp = function () {
+  return this.toString().gf().jp();
+};
+String.prototype.gfdp = function (param) {
+  return this.toString().gf().dp(param);
+};
+String.prototype.query = function (callback) {
+  const sql = this.toString();
+  const dbconf = "db.json";
+  const connection = mysql.createConnection(dbconf.gfjp());
+  connection.connect();
+  connection.query(sql, callback);
+  connection.end();
+};
+String.prototype.howmany = function (str) {
+  let num = this.match(new RegExp(str, "g"));
+  if (!num) num = 0;
+  else num = num.length;
+  return num;
+};
 const golfClubEngNames = [];
 const golfClubIdToEng = {};
 const golfClubIds = {};
@@ -1584,43 +1625,4 @@ function gf(file) {
   return fs.readFileSync(file, "utf-8");
 }
 
-String.prototype.dp = function (param) {
-  let self = this;
-  const keys = Object.keys(param);
-  keys.forEach((key) => {
-    const regex = new RegExp("\\$\\{".add(key).add("\\}"), "g");
-    const val = param[key];
-    self = self.replace(regex, val);
-  });
-  return self;
-};
-String.prototype.add = function add(str) {
-  return [this, str].join("");
-};
-String.prototype.jp = function () {
-  return JSON.parse(this);
-};
-String.prototype.gf = function () {
-  const path = this.toString();
-  return fs.readFileSync(path, "utf-8");
-};
-String.prototype.gfjp = function () {
-  return this.toString().gf().jp();
-};
-String.prototype.gfdp = function (param) {
-  return this.toString().gf().dp(param);
-};
-String.prototype.query = function (callback) {
-  const sql = this.toString();
-  const dbconf = "db.json";
-  const connection = mysql.createConnection(dbconf.gfjp());
-  connection.connect();
-  connection.query(sql, callback);
-  connection.end();
-};
-String.prototype.howmany = function (str) {
-  let num = this.match(new RegExp(str, "g"));
-  if (!num) num = 0;
-  else num = num.length;
-  return num;
-};
+
