873399b0d7749e2f2b88f0a264e399d2b0621bbb
jaehyunlee <jaehyunlee25@gmail.com>
2022-10-25 14:27:06 +0900

    'renew'

--- sql/setQuestion.sql
+++ sql/setQuestion.sql
@@ -1,10 +1,10 @@
 insert into question values(
     uuid(),
-    `${type}`,
-    `${name}`,
-    `${email}`,
-    `${subject}`,
-    `${content}`,
+    "${type}",
+    "${name}",
+    "${email}",
+    "${subject}",
+    "${content}",
     now(),
     now()
 );
