fac479036314dccf41d9b6c384f0213634027e28
jaehyunlee <jaehyunlee25@gmail.com>
2022-10-25 16:20:24 +0900

    'renew'

--- operation.js
+++ operation.js
@@ -6,11 +6,53 @@ String.prototype.ct = function (num) {
   return this.substring(0, this.length - num);
 };
 
-const path = "./script/reserve_core/reserve/";
+const path = "./script/reserve_core/search/";
 const files = fs.readdirSync(path);
-files.forEach((file) => {
+let cnt = 0;
+files.forEach((file, i) => {
+  if (file.indexOf(".js") != -1) return;
   if (file.indexOf(".") != -1) return;
+  cnt++;
   const addr = path + file + "/funcs.json";
   const con = fs.readFileSync(addr, "utf-8");
-  fs.writeFileSync("script/search_logout/" + file + ".json", con, "utf-8");
+  const funcs = JSON.parse(con);
+
+  const funcName = funcs.funcSearch ? "funcSearch" : "funcReserve";
+  const lines = funcs[funcName].split("\r\n");
+
+  log(funcs[funcName]);
+  return;
+
+  let flg = false;
+  let chk = false;
+  const before = [];
+  const param = [];
+  const after = [];
+  lines.forEach((ln) => {
+    if (ln.indexOf("const addr = OUTER_ADDR_HEADER") != -1) {
+      log(cnt, file, funcName);
+      flg = true;
+      chk = true;
+    }
+    if (flg) {
+      param.push(ln);
+    } else {
+      if (chk) after.push(ln);
+      else before.push(ln);
+    }
+
+    if (flg && ln.trim() == "});") flg = false;
+  });
+
+  if (param[0].indexOf("/*") == -1) {
+    param.unshift("    /*");
+    param.push("    */");
+    param.unshift("    if(ac) ac.message(JSON.stringify(param));");
+  } else {
+  }
+
+  funcs[funcName] = [...before, ...param, ...after].join("\r\n");
+  log(funcs[funcName]);
+
+  //fs.writeFileSync(addr, JSON.stringify(funcs), "utf-8");
 });
---
+++ script/reserve_core/search_old_3/search_common.js
@@ -0,0 +1,59 @@
+${commonScript}
+
+/* begin blocking infinite call */
+let TZ_BOT_SAFETY = true;
+let visitNumber = lsg("TZ_ADMIN_BLOCK_IC") * 1;
+let lastVistTime = lsg("TZ_ADMIN_BLOCK_IC_TIME") * 1;
+let curTimeforVisit = new Date().getTime();
+log(visitNumber, visitNumber == null);
+if(lsg("TZ_ADMIN_BLOCK_IC") != null) {
+	log(1);
+	log(curTimeforVisit, lastVistTime, curTimeforVisit - lastVistTime, curTimeforVisit - lastVistTime < 1000 * 15);
+	if (curTimeforVisit - lastVistTime < 1000 * 15) {
+		log(2);
+		if (visitNumber > 9) {
+			log(3);
+			if(window.AndroidController) 
+				window.AndroidController.message("TZ_MSG_IC");
+			TZ_BOT_SAFETY = false;
+			/* 초기화 */
+			visitNumber = 0;
+			lss("TZ_ADMIN_BLOCK_IC_TIME", new Date().getTime());
+			/* 로그아웃 */
+			if (LOGOUT) LOGOUT();
+		}
+	} else {
+		log(4);
+		visitNumber = 0;
+		lss("TZ_ADMIN_BLOCK_IC_TIME", new Date().getTime());
+	}
+} else {
+	log(5);
+	visitNumber = 0;
+	lss("TZ_ADMIN_BLOCK_IC_TIME", new Date().getTime());
+}
+visitNumber++;
+lss("TZ_ADMIN_BLOCK_IC", visitNumber);
+log("TZ_ADMIN_BLOCK_IC", lsg("TZ_ADMIN_BLOCK_IC"), lsg("TZ_ADMIN_BLOCK_IC_TIME"));
+/* end blocking infinite call */
+
+/* const splitter = location.href.indexOf("?") == -1 ? "#" : "?";
+const aDDr = location.href.split(splitter)[0];
+const suffix = location.href.split(splitter)[1];
+const dictSplitter = {"#": "?", "?": "#"};
+let addr = aDDr;
+if(aDDr.indexOf(dictSplitter[splitter]) != -1) 
+    addr = aDDr.split(dictSplitter[splitter])[0]; */
+
+log("raw addr :: ", location.href);
+log("aDDr :: ", aDDr);
+log("addr :: ", addr);
+    
+const dict = ${address_mapping};
+
+const func = dict[addr];
+const dictCourse = ${reserve_course_mapping};
+
+if (!func) funcOther();
+else func();
+
