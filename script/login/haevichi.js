873095f0656eaffae854260b40f6da9605f37ad5
jaehyunlee <jaehyunlee25@gmail.com>
2022-10-26 13:40:05 +0900

    'renew'

--- script/login/haevichi.js
+++ script/login/haevichi.js
@@ -1,3 +1,8 @@
-memberID.value = '${login_id}';
-memberPassword.value = '${login_password}';
-loginSubmit.click();
+const loginT = setInterval(() => {
+  if (!window["memberId"]) return;
+
+  clearInterval(loginT);
+  memberID.value = "${login_id}";
+  memberPassword.value = "${login_password}";
+  loginSubmit.click();
+}, 200);
