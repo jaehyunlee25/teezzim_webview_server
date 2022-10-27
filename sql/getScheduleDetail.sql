9f9071df95a7953a937917c2465373330df70196
jaehyunlee <jaehyunlee25@gmail.com>
2022-10-27 13:57:05 +0900

    'renew'

--- sql/getScheduleDetail.sql
+++ sql/getScheduleDetail.sql
@@ -2,7 +2,9 @@ select
     golf_club.id club_id,
     golf_club.name club_name,
     golf_course.id course_id,
-    golf_course.name course_name
+    golf_course.name course_name,
+    '1234'
 from golf_club
 join golf_course on golf_club.id = golf_course.golf_club_id
-order by golf_club.name asc;
+where golf_club.id = '${golf_club_id}'
+and golf_course.name in ('${golf_course}');
