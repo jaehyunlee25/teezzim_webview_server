f132b5af105dfb9a3923b623653cd3eb7e3fd29b
jaehyunlee <jaehyunlee25@gmail.com>
2022-10-27 13:57:39 +0900

    'renew'

--- sql/getScheduleDetail.sql
+++ sql/getScheduleDetail.sql
@@ -5,6 +5,6 @@ select
     golf_course.name course_name,
     '1234'
 from golf_club
-join golf_course on golf_club.id = golf_course.golf_club_id
-where golf_club.id = '${golf_club_id}'
-and golf_course.name in ('${golf_course}');
+join golf_course on golf_club.id = golf_course.golf_club_id;
+/* where golf_club.id = '${golf_club_id}'
+and golf_course.name in ('${golf_course}'); */
