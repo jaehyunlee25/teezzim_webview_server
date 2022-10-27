e4ea78ef3ddd5142d251d161d27937fecb32ad04
jaehyunlee <jaehyunlee25@gmail.com>
2022-10-27 13:52:51 +0900

    'renew'

--- sql/getScheduleDetail.sql
+++ sql/getScheduleDetail.sql
@@ -1,1 +1,8 @@
-select * from golf_club order by name asc;
+select 
+    golf_club.id,
+    golf_club.name,
+    golf_course.id,
+    golf_course.name
+from golf_club
+join golf_course on golf_club.id = golf_course.golf_club_id
+order by golf_club.name asc;
