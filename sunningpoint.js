dc4c7c89105be67842235a4bc35e2c13e78bc9ad
jaehyunlee <jaehyunlee25@gmail.com>
2022-11-15 14:16:56 +0900

    'renew'

--- sunningpoint.js
+++ sunningpoint.js
@@ -379,6 +379,7 @@ javascript: (() => {
     return;
   }
 
+  /* begin blocking infinite call */
   let TZ_BOT_SAFETY = true;
   let visitNumber = lsg("TZ_ADMIN_BLOCK_IC") * 1;
   let lastVistTime = lsg("TZ_ADMIN_BLOCK_IC_TIME") * 1;
