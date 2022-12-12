61b82f8781ed2de38094bf5df127831595f1a50b
jaehyunlee <jaehyunlee25@gmail.com>
2022-12-12 16:35:21 +0900

    'renew'

--- teezzim.js
+++ teezzim.js
@@ -279,7 +279,7 @@ function procPost(request, response, data) {
     });
     objResp = 0;
   } else if (reqUrl == "/dbSetGolfClub") {
-    "sql/getOuterInfo.sql".gfdp(data).query((err, rows, fields) => {
+    "sql/setDbGolfClub.sql".gfdp(data).query((err, rows, fields) => {
       if (err) {
         objResp = {
           type: "error",
