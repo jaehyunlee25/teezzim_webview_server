a244415f251429b6454641347aa1c0ec0ffd024f
jaehyunlee <jaehyunlee25@gmail.com>
2022-12-19 21:09:06 +0900

    'renew'

--- teezzim.js
+++ teezzim.js
@@ -279,6 +279,26 @@ function procPost(request, response, data) {
     });
     objResp = 0;
   } else if (reqUrl == "/dbCheckServerfile") {
+    const { eng_id: eng } = data;
+    const arRes = [
+      ["login_url", golfClubLoginUrl[eng], "funcLogin"],
+      ["search_url", golfClubSearchUrl[eng], "funcReserve"],
+    ];
+    const arObj = {
+      LOGOUT:
+        '  function LOGOUT() {\r\n    log("LOGOUT");\r\n    location.href="javascript:()=>{}";\r\n  }',
+    };
+    fs.writeFileSync(
+      "/script/search_dict/" + eng + ".json",
+      JSON.stringify(arRes),
+      "utf-8"
+    );
+    fs.writeFileSync(
+      "/script/search_logout/" + eng + ".json",
+      JSON.stringify(arObj),
+      "utf-8"
+    );
+  } else if (reqUrl == "/dbCheckServerfile") {
     const eng = data.eng_id;
     objResp = {
       type: "okay",
