2a3a2940adff2c671ff69bb7697f03614460dcfc
jaehyunlee <jaehyunlee25@gmail.com>
2022-12-26 17:13:07 +0900

    'renew'

--- teezzim.js
+++ teezzim.js
@@ -1079,6 +1079,33 @@ function procPost(request, response, data) {
       procMessage: proc ? proc.message : "",
       procLandingLink: proc ? proc.landingLink : "",
     };
+  } else if (reqUrl == "/loginScripts") {
+    const ids = data.clubIds;
+    const urls = {};
+    const scripts = {};
+    const procProcs = {};
+    const procLandingLinks = {};
+    const procMessages = {};
+    const procResults = {};
+    ids.forEach((uuid) => {
+      const engName = golfClubIdToEng[uuid];
+      urls[uuid] = golfClubLoginUrl[engName];
+      scripts[uuid] = getLoginScript(engName);
+      proc = golfClubLoginProc[uuid];
+      procProcs[uuid] = proc ? proc.proc : "";
+      procLandingLinks[uuid] = proc ? proc.landingLink : "";
+      procMessages[uuid] = proc ? proc.message : "";
+      procResults[uuid] = proc ? proc.result : "";
+    });
+
+    objResp = {
+      urls,
+      scripts,
+      procProcs,
+      procLandingLinks,
+      procMessages,
+      procResults,
+    };
   } else {
     const engName = reqUrl.substring(1);
     url = golfClubLoginUrl[engName];
