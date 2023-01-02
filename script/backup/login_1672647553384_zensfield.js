681c046856d7faab7ee3e08113a4147d989b15db
jae hyun lee <jaehyunlee25@gmail.com>
2023-01-02 08:19:21 +0000

    renew

---
+++ script/backup/login_1672647553384_zensfield.js
@@ -0,0 +1,14 @@
+var tLoginCount = 0;
+log("tLoginCount", tLoginCount);
+const tLogin = setInterval(() => {
+  if (!window["login_id"]) {
+    tLoginCount++;
+    log("tLoginCount", tLoginCount);
+    return;
+  }
+  clearInterval(tLogin);
+  if (precheck()) return;
+  login_id.value = "${login_id}";
+  login_pw.value = "${login_password}";
+  chkLogValue("frmLogin", "in", "login_id", "login_pw");
+}, 1000);
