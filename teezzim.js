aef9ff1a3c16464cf5a1add2fffd06af27e01029
jae hyun lee <jaehyunlee25@gmail.com>
2023-01-07 01:36:00 +0900

    'renew'

--- teezzim.js
+++ teezzim.js
@@ -48,7 +48,6 @@ String.prototype.query = function (callback) {
   try {
     const sql = this.toString();
     const dbconf = "db.json";
-    dir(dbconf.gfjp());
     const connection = mysql.createConnection(dbconf.gfjp());
     connection.connect();
     connection.query(sql, callback);
