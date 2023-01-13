c592425148345c448219b701e9d8d9977045ee12
jaehyunlee <jaehyunlee25@gmail.com>
2023-01-13 11:07:16 +0900

    'renew'

--- sql/getLog.sql
+++ sql/getLog.sql
@@ -3,4 +3,6 @@ select
 from LOG 
 where 
     DATE(created_at) = '${date}'
-    and device_id = '${device_id}';
+    and device_id = '${device_id}'
+    and golf_club_id = '${golf_club_id}'
+order by created_at asc;
