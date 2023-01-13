9b54517fcf064c1d4b8578b763a9095ef2918421
jaehyunlee <jaehyunlee25@gmail.com>
2023-01-13 10:48:37 +0900

    'renew'

--- teezzim.js
+++ teezzim.js
@@ -261,6 +261,22 @@ function procPost(request, response, data) {
       response.end();
     });
     objResp = 0;
+  } else if (reqUrl == "/getLogClubList") {
+    "sql/getLogClubList.sql".gfdp(data).query((err, rows, fields) => {
+      if (err) {
+        objResp = {
+          type: "error",
+          data: err,
+        };
+      } else {
+        objResp = {
+          type: "okay",
+          data: rows,
+        };
+      }
+      response.write(JSON.stringify(objResp));
+      response.end();
+    });
   } else if (reqUrl == "/getLogDeviceList") {
     "sql/getLogDeviceList.sql".gfdp(data).query((err, rows, fields) => {
       if (err) {
