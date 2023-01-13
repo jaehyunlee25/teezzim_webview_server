dabaf6026c5c30e9b5f54b7323b0cbdbe205419e
jaehyunlee <jaehyunlee25@gmail.com>
2023-01-13 22:35:37 +0900

    'renew'

--- sql/getLog.sql
+++ sql/getLog.sql
@@ -3,7 +3,7 @@ select
     MICROSECOND(created_at) timestamp
 from LOG 
 where 
-    DATE(CONVERT_TZ(created_at, '+00:00', '+09:00')) = '${date}'
+    DATE(created_at) = '${date}'
     and device_id = '${device_id}'
     and golf_club_id = '${golf_club_id}'
 order by created_at asc;
