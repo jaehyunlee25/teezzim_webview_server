15b5a0e318a344ec95d6f93fecabbfbecda10ff9
jaehyunlee <jaehyunlee25@gmail.com>
2023-01-13 10:10:35 +0900

    'renew'

--- sql/getLog.sql
+++ sql/getLog.sql
@@ -1,1 +1,4 @@
-select * from LOG where DATE(created_at) = '${date}';
+select * from LOG 
+where 
+    DATE(created_at) = '${date}'
+    and device_id = '${device_id}';
---
+++ sql/getLogDeviceList.sql
@@ -0,0 +1,6 @@
+select
+    distinct(device_id)
+from
+    LOG
+where
+    DATE(created_at) = '${date}';
