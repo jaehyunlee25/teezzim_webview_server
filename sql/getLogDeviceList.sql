06c1b1eb3bdacbfbea24d11139587b7af91c64a3
jaehyunlee <jaehyunlee25@gmail.com>
2023-01-16 17:26:36 +0900

    'renew'

--- sql/getLogDeviceList.sql
+++ sql/getLogDeviceList.sql
@@ -3,6 +3,6 @@ select
 from
     LOG
 where
-    DATE(CONVERT_TZ(created_at, '+00:00', '+09:00')) = '${date}'
+    DATE(created_at) = '${date}'
     and device_id != '${deviceId}'
 order by created_at asc;
