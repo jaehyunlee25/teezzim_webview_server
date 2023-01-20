1790f003f795b915729aa3877626685397fee069
jaehyunlee <jaehyunlee25@gmail.com>
2023-01-20 23:19:21 +0900

    'renew'

--- sql/setDbGolfClubOuterInfo.sql
+++ sql/setDbGolfClubOuterInfo.sql
@@ -1,6 +1,5 @@
 update golf_club_outer_info
 set
     weather_location_id = "${weather_location_id}",
-    kakao_endLoc = "${kakao_endLoc}",
-    updated_at = now()
+    kakao_endLoc = "${kakao_endLoc}"
 where golf_club_id = "${id}";
