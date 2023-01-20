2acc554e19dc0c86ba5e86c77ebe826990a6225a
jaehyunlee <jaehyunlee25@gmail.com>
2023-01-20 22:56:49 +0900

    'renew'

--- sql/getGolfClub.sql
+++ sql/getGolfClub.sql
@@ -1,1 +1,6 @@
-select * from golf_club;
+select 
+    golf_club.*,
+    golf_club_outer_info.weather_location_id,
+    golf_club_outer_info.kakao_endLoc
+from golf_club
+join golf_club_outer_info on golf_club.id = golf_club_outer_info.golf_club_id;
